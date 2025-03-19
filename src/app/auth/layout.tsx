cf889627eb60eb15160aceec0026e3d6bef3423f
David Etuk <djetuk@gmail.com>
2025-03-19 12:35:07 +0100

    refined project structure w landing page

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,14 @@
+import "@radix-ui/themes/styles.css";
+import "./globals.css";
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
