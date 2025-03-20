2cac4f3e3dfecd5d8c5c8eb0a0e08d9da0dae3a8
David Etuk <djetuk@gmail.com>
2025-03-20 22:31:11 +0100

    added redux store

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-import StoreProvider from "./store-provider";
+import { StoreProvider } from "./store-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
