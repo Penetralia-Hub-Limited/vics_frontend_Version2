c146fe0db2b61b9ab5217b073633024c5d9f7654
David Etuk <djetuk@gmail.com>
2025-03-22 00:32:05 +0100

    added notifications component

--- src/app/(dashboard)/mla-admin/page.tsx
+++ src/app/(dashboard)/mla-admin/page.tsx
@@ -1,7 +1,11 @@
+import { DashboardNotificationsComp } from "@/components/dashboard/notification/dashboard-notifications";
+
 export default function Page() {
   return (
     <main>
       <div>MLA Dashboard</div>
+
+      <DashboardNotificationsComp className={"p-0"} />
     </main>
   );
 }
