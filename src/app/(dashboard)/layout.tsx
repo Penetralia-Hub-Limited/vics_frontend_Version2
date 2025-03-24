ef6cbe1bbec7149cfa0063c17566e1557aa4a37b
Bernard Iorver <166822784+benion88@users.noreply.github.com>
2025-03-24 10:31:00 +0100

    Reversed Main Repository Changes

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,14 @@
+import "@radix-ui/themes/styles.css";
+import "./globals.css";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>{children}</main>;
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
