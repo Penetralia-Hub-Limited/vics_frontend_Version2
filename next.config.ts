d9b8ad3325c9cd37496c395dbc7053d933f6a373
David Etuk <djetuk@gmail.com>
2025-04-28 19:44:53 +0100

    re-added turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.ts",
-        },
-      },
-      resolveExtensions: [
-        ".mdx",
-        ".tsx",
-        ".ts",
-        ".jsx",
-        ".js",
-        ".mjs",
-        ".json",
-      ],
-    },
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.com",
-        port: "",
-        search: "",
-      },
-    ],
-  },
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      html2canvas: "html2canvas-pro",
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
