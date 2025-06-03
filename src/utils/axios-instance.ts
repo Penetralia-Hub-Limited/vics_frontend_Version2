42a38d37b0de68e58d5d11b207a14e7f8530b3bd
MGobir <mgobir10@gmail.com>
2025-06-03 13:40:56 +0100

    Update axios-instance.ts

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -14,6 +14,7 @@ const axiosInstance = axios.create({
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
   },
+   withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
