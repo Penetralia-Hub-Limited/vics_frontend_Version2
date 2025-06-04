3d9e912191d7d187488b63e46f2893dd8e89b0d0
MGobir <mgobir10@gmail.com>
2025-06-04 10:37:44 +0100

    Update axios-instance.ts

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -10,7 +10,10 @@ const axiosInstance = axios.create({
   timeout: 120000,
   headers: {
     "Content-Type": "application/json",
-    "Accept": "*/*",  
+    "Accept": "*/*",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
   },
 });
 
